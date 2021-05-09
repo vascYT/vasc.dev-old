585318e0d5d88e88b232311af0f6bd5baa9d5be1
vasc <contact@vasc.dev>
2021-05-09 03:52:23 +0200

    Remove blank space in activity when information is not given.

--- components/Activity.js
+++ components/Activity.js
@@ -4,7 +4,6 @@ import { useLanyard } from "use-lanyard";
     Thanks to https://github.com/iGalaxyYT/igalaxy.dev/blob/main/components/Lanyard.tsx for the help :)
 */
 
-const fetcher = url => fetch(url, {headers: {"Origin": "https://vasc.dev"}}).then(r => r.json())
 
 export default function Lanyard() {
     const { data: activity } = useLanyard("346977366569910274");
@@ -29,34 +28,29 @@ export default function Lanyard() {
         
     // Playing a game
     if (activity.activities.length != 0) {
-        return (
-            <div className="flex space-x-2">
-                <div className="animate-pulse h-full w-1 rounded-full bg-green-400"></div>
-                <div>
-                    <span className="font-bold">Playing {activity.activities[0].name}</span>
-                    <br />
-                    <span className="text-gray-400">{activity.activities[0].details}</span>
-                    <br />
-                    <span className="text-gray-400">{activity.activities[0].state}</span>
+        if (activity.activities[0].details) {
+            return (
+                <div className="flex space-x-2">
+                    <div className="animate-pulse h-full w-1 rounded-full bg-green-400"></div>
+                    <div>
+                        <span className="font-bold">Playing {activity.activities[0].name}</span>
+                        <br />
+                        <span className="text-gray-400">{activity.activities[0].details}</span>
+                        <br />
+                        <span className="text-gray-400">{activity.activities[0].state}</span>
+                    </div>
                 </div>
-            </div>
-        );
-    }
-
-    // Mining
-    if (activity.activities.length != 0) {
-        return (
-            <div className="flex space-x-2">
-                <div className="animate-pulse h-full w-1 rounded-full bg-green-400"></div>
-                <div>
-                    <span className="font-bold">Playing {activity.activities[0].name}</span>
-                    <br />
-                    <span className="text-gray-400">{activity.activities[0].details}</span>
-                    <br />
-                    <span className="text-gray-400">{activity.activities[0].state}</span>
+            );   
+        } else {
+            return (
+                <div className="flex space-x-2">
+                    <div className="animate-pulse h-full w-1 rounded-full bg-green-400"></div>
+                    <div>
+                        <span className="font-bold">Playing {activity.activities[0].name}</span>
+                    </div>
                 </div>
-            </div>
-        );
+            );
+        }
     }
 
     return null;
