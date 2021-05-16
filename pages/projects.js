99bb56ab9f8b8f0726245e91a5f22330466cf312
vasc <contact@vasc.dev>
2021-05-17 01:59:55 +0200

    Added presencedb to projects

--- pages/projects.js
+++ pages/projects.js
@@ -11,6 +11,7 @@ export default function Projects() {
             <PageContainer>
                 <h1 className="text-4xl sm:text-5xl font-bold bg-gradient-to-r from-yellow-400 to-yellow-600 bg-clip-text text-transparent">Projects</h1>
                 <ul className="mt-5 space-y-1.5">
+                    <ProjectItem name="PresenceDB" href="https://presencedb.vercel.app" description="A service that tracks the playtime of your Discord Activities."/>
                     <ProjectItem name="SpeedUHC" href="https://github.com/vascYT/SpeedUHC-Plugin" description="A Minecraft Plugin for the popular SpeedUHC gamemode."/>
                     <ProjectItem name="Insaneus" href="https://top.gg/bot/758389658286817340" description="A multifunctional Discord Bot."/>
                     <ProjectItem name="CS:GO Tracker" href="https://top.gg/bot/770019524614815745" description="A Discord Bot that tracks the price of CS:GO items."/>
