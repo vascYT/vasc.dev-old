989bf05856a1f59a855fef61304b47dca8b8320f
vasc <nicovanaschaufler@gmail.com>
2021-07-20 15:26:35 +0200

    Added splitbee, added yarn

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,9 @@
 import "tailwindcss/tailwind.css";
 import "../styles/global.css";
+import splitbee from "@splitbee/web";
+
+splitbee.init();
 
 export default function App({ Component, pageProps }) {
-    return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
