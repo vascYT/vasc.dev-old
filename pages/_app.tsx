327440461c63212e65e6413370e2a68ab5209912
vasc <nicovanaschaufler@gmail.com>
2021-10-17 17:04:02 +0200

    Switch to GA

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,20 +1,14 @@
 import { AppProps } from "next/app";
-import Head from "next/head";
 import "tailwindcss/tailwind.css";
 import "../styles/global.css";
+import ReactGA from "react-ga";
+import { useEffect } from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <>
-      <Head>
-        <script
-          async
-          defer
-          data-website-id="d38f0e10-afae-4110-89e9-12cacc0b6902"
-          src="https://analytics.vasc.dev/umami.js"
-        />
-      </Head>
-      <Component {...pageProps} />
-    </>
-  );
+  useEffect(() => {
+    ReactGA.initialize("UA-150201105-4");
+    ReactGA.pageview(window.location.pathname + window.location.search);
+  });
+
+  return <Component {...pageProps} />;
 }
